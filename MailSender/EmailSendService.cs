bfa60ed3455412aeb49d7aeec92c0049c48690ed
g0shafedorof@yandex.ru <g0shafedorof@yandex.ru>
2020-01-31 00:03:56 +0300

    добавил сервис по отправке сообщение и сделал тестовый пример (по заданию 1 из методлички). Стили не скачал, Chrome ругается на ссылку

---
+++ MailSender/EmailSendService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSender
+{
+    public class EmailSendService
+    {
+        /// <summary>
+        /// Адрес SMTP сервера
+        /// </summary>
+        public string SmtpAddress { get; private set; }
+        /// <summary>
+        /// SMTP порт
+        /// </summary>
+        public int SmtpPort { get; private set; }
+        /// <summary>
+        /// Адрес исходящей почты
+        /// </summary>
+        public string OutgoingMail { get; private set; }
+        /// <summary>
+        /// Пароль исходящей почты
+        /// </summary>
+        public string OutgoingMailPassword { get; private set; }
+
+        /// <summary>
+        /// Сервис отправки сообщений
+        /// </summary>
+        /// <param name="smtpAddress">адрес SMTP сервера</param>
+        /// <param name="smtpPort">SMTP порт</param>
+        /// <param name="outgoingMail">адрес исходящей почты</param>
+        /// <param name="outgoingMailPassword">пароль исходящей почты</param>
+        public EmailSendService(string smtpAddress, int smtpPort, string outgoingMail, string outgoingMailPassword)
+        {
+            SmtpAddress = smtpAddress;
+            SmtpPort = smtpPort;
+            OutgoingMail = outgoingMail;
+            OutgoingMailPassword = outgoingMailPassword;
+        }
+
+        /// <summary>
+        /// Отправить сообщение
+        /// </summary>
+        /// <param name="mail">адрес отправки</param>
+        /// <param name="text">текст сообщения</param>
+        public void SendTo(string mail, string text) => SendTo(mail, text, "");
+
+        /// <summary>
+        /// Отправить сообщение
+        /// </summary>
+        /// <param name="mail">адрес отправки</param>
+        /// <param name="text">текст сообщения</param>
+        /// <param name="subject">заголовок сообщения</param>        
+        public void SendTo(string mail, string text, string subject)
+        {
+            if (string.IsNullOrEmpty(mail))
+                throw new Exception("Адрес отправки не указан");
+
+            using (MailMessage msg = new MailMessage(OutgoingMail, mail))
+            {
+                // формирование письма
+                msg.Subject = subject;      // Заголовок письма
+                msg.Body = text;            // Тело письма
+                
+                msg.IsBodyHtml = false;     // html не используется в теле письма
+
+                // Авторизация на smtp-сервере и отправление письма
+                using (SmtpClient sc = new SmtpClient(SmtpAddress, SmtpPort))
+                {
+                    sc.EnableSsl = true;
+                    sc.Credentials = new NetworkCredential(OutgoingMail, OutgoingMailPassword);
+
+                    sc.Send(msg);
+                }
+            }
+        }
+    }
+}
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -28,43 +28,29 @@ namespace MailSender
         }
 
         private void button_SendEmail_Click(object sender, RoutedEventArgs e)
-        {
-            // Список email'ов кому мы отправляем письмо
-            List<string> listStrMails = new List<string> { "g0shafedorof@yandex.ru" };
-
+        {   
             string strPassword = passwordBox_PasswordOwnMail.Password;
 
-            foreach (string mail in listStrMails)
-            {
-                // Используем using, чтобы гарантированно удалить объект MailMessage после использования
-                using (MailMessage mm = new MailMessage("goshafedorof@mail.ru", mail))
+            EmailSendService service = new EmailSendService(Data.SMTP_MAIL, Data.SMTP_PORT, Data.OUTGOING_MAIL, strPassword);
+
+            foreach (string mail in Data.Mails)
+                try
                 {
-                    // Формируем письмо
-                    mm.Subject = "Привет из C#";        // Заголовок письма
-                    mm.Body = "Hello, world!";          // Тело письма
-                    mm.IsBodyHtml = false;              // Не используем html в теле письма
-
-                    // Авторизуемся на smtp-сервере и отправляем письмо
-                    // Оператор using гарантирует вызов метода Dispose, даже если при вызове
-                    // методов в объекте происходит исключение.
-
-                    using (SmtpClient sc = new SmtpClient("smtp.mail.ru", 25))
-                    {
-                        sc.EnableSsl = true;
-                        sc.Credentials = new NetworkCredential("goshafedorof@mail.ru", strPassword);
-
-                        try
-                        {
-                            sc.Send(mm);
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show(" Невозможно отправить письмо " + ex.ToString());
-                        }
-                    }
+                    service.SendTo(mail, $"{DateTime.Now} :: Текст для примера", $"{DateTime.Now} :: ЗАГОЛОВОК");
+                    MessageBox.Show(
+                        $"Сообщение на адрес {mail} доставлено!", 
+                        "Сообщение доставлено",
+                        MessageBoxButton.OK, 
+                        MessageBoxImage.Information);
                 }
-            }
-            MessageBox.Show("Работа завершена.");
+                catch (Exception exc)
+                {
+                    MessageBox.Show(
+                        $"Ошибка отправления:{Environment.NewLine}\"{exc}\"", 
+                        "Ошибка!", 
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                }
         }
     }
 }
