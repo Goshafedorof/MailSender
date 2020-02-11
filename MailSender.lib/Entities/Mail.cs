679dd0b39b8485be1f5fc59682c793b291f4bda2
g0shafedorof@yandex.ru <g0shafedorof@yandex.ru>
2020-02-11 21:49:25 +0300

    тестирование pullRequest

--- MailSender.lib/Entities/Mail.cs
+++ MailSender.lib/Entities/Mail.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace MailSender.lib.Entities
 {
+    /// <summary>
+    /// Тестирование особенностей pullRequest
+    /// </summary>
     public class Mail : BaseEntity
     {
         /// <summary>
