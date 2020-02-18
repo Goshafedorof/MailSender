3c12401afeefbf885ad56579cc37671af933a0c6
g0shafedorof@yandex.ru <g0shafedorof@yandex.ru>
2020-02-18 14:05:52 +0300

    Написал метод перемножения матриц, не могу запустить unit-test

---
+++ TPLProjects/MatrixMultiplaction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Даны две двумерных матрицы размерностью 100х100 каждая.Задача: написать приложение,
+// производящее их параллельное умножение. Матрицы заполняются случайными целыми
+// числами от 0 до 10.
+namespace MatrixMultiplaction
+{
+    class Program
+    {
+        static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность квадратной матрицы:");
+            Console.Write("N = ");
+
+            // считывание параметров
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Matrix a = Matrix.GetRandom(n, n);
+            Matrix b = Matrix.GetRandom(n, n);
+
+            Matrix c = Matrix.Multiply(a, b);
+
+            Console.WriteLine();
+            Console.Write("Для завершения работы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+
+    }
+}
