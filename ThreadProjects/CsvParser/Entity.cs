72ab7698a61227b4e6077d98764c624d3353fb69
g0shafedorof@yandex.ru <g0shafedorof@yandex.ru>
2020-02-18 11:57:56 +0300

    Доделать CsvParser

---
+++ ThreadProjects/CsvParser/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvParser
+{
+    public class Entity
+    {
+        private readonly static string[] SEPARATORS = new string[] { ",", ".", ";", ":" };
+
+        /// <summary>
+        /// Колонки, которые описывают структуру сущности
+        /// </summary>
+        public List<string> Properties { get; set; } = new List<string>();
+
+        public static Entity Parse(string line) => new Entity() { Properties = new List<string>(line.Split(SEPARATORS, StringSplitOptions.RemoveEmptyEntries).AsEnumerable()) };        
+    }
+}
--- ThreadProjects/CsvParser/Program.cs
+++ ThreadProjects/CsvParser/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+// Написать приложение, выполняющее парсинг CSV-файла произвольной структуры и
+// сохраняющего его в обычный txt-файл.Все операции проходят в потоках.CSV-файл заведомо
+// имеет большой объем.
+
+// Формат .csv — это файл, в котором данные разделены между собой, используя конркетный символ
 
 namespace CsvParser
 {
     class Program
     {
+        const string FILENAME = "data.csv";
+
         static void Main(string[] args)
         {
+            // создаем тестовый файл
+            ModelCsvFile.GetFile(FILENAME);
+
+
         }
     }
 }
